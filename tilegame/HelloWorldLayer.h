949b5f82eea66b2356b0311ed9155e1f79ed6eb6
mooosu <mooosu@hotmail.com>
2013-11-30 23:03:59 +0800

    import tilegame

---
+++ tilegame/HelloWorldLayer.h
@@ -0,0 +1,18 @@
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+@interface HudLayer : CCLayer
+- (void)numCollectedChanged:(int)numCollected;
+@end
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
